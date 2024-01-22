8af47066ffdfe266df8e6f3f50b51ceb51a13a4a
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-01-22 11:29:25 +0530

    files added

--- js_files/products_index.js
+++ js_files/products_index.js
@@ -21,7 +21,7 @@ fetch("./json_data/products.json")
         for (let i = 0; i < y.length; i++) {
             y[i].addEventListener("click", function (){
                 let title = document.querySelector(`#p${i}`).innerHTML;
-                console.log(title)  
+                console.log(String(title))  
                 handleFilter(arr,title)
             })
         }
